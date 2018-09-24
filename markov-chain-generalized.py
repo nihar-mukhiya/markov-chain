a43828b53d8799c2cda22f4605976b0868a55c0a
nihar-mukhiya <niharmukhiya@gmail.com>
2018-09-24 23:09:03 +0530

    had forgot to generalize column values to list

--- markov-chain-generalized.py
+++ markov-chain-generalized.py
@@ -1,3 +1,9 @@
+# author: NIHAR MUKHIYA
+# date: 20/09/2018
+# last update: 24/09/2018
+# description: Markov chain implementaion in python,
+#              where dataset inputted by user in form of csv file.
+
 import tkinter as tk
 from tkinter import filedialog
 from datascience import *
@@ -6,10 +12,8 @@ import numpy as np
 
 root = tk.Tk()
 
-
 file_path = filedialog.askopenfilename()
 file = Table.read_table(file_path)
-
 
 a = Table.read_table(file_path)
 e = pandas.read_csv(file_path, header = 0)
@@ -17,9 +21,11 @@ c = a.num_rows
 column_names = a.labels
 column = column_names[0]
 z = e[column].tolist()
-#z = e.columns.values.tolist()
+# eset consists of unique column values
 eset = list(set(z))
+print("unique column values are: ")
 print(eset)
+print("name of column is: ")
 print(column_names)
 list = []
 i = 0
@@ -27,7 +33,6 @@ for x in eset:
     t= a.select(column).where(column, eset[i]).num_rows
     list.append(t)
     i+=1
-    print(list)
 list2 = []
 
 list2 = list.copy()
